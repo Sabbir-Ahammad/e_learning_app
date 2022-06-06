8405884452d061ffa45860a635394a55411a85b6
ahsunhabib96 <94116363+Sabbir-Ahammad@users.noreply.github.com>
2022-06-06 18:49:59 +0600

    june062022

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:e_learning_app/constraints/color.dart';
+import 'package:e_learning_app/screens/home/widget/emoji_text.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,7 +7,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Homepage')),
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [EmojiText()],
+      )),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: kBackground,
+      elevation: 0,
+      centerTitle: false,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Text(
+          'Hello Ruize',
+          style: TextStyle(fontSize: 16, color: kFontLight
+        ),
+        ),
+      ),
+      actions: [
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10, right: 20),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(color: kFontLight.withOpacity(0.3), width: 2),
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: Image.asset(
+                'assets/icons/notification.png',
+                width: 30,
+              ),
+            ),
+            Positioned(
+              top: 15,
+              right: 30,
+              child: Container(
+                height: 8,
+                width: 8,
+                decoration: BoxDecoration(color: kAccent, shape: BoxShape.circle),
+              ))
+          ],
+        )
+      ],
     );
   }
 }
---
+++ lib/screens/home/widget/emoji_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EmojiText extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Emoji Text'),
+    );
+  }
+}
